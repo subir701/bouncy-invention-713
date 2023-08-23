84d608a83b915a36c4bc8c325ec4a1581461a529
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-08-23 20:49:32 +0530

    Entities

---
+++ WonderWorld_Park_BK/src/main/java/com/masai/model/Activity.java
@@ -0,0 +1,56 @@
+package com.masai.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class Activity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer activityId;
+	
+	@Column(unique = true, nullable = false)
+	private String activityName;
+	
+	private Double price;
+	
+	private Integer personCapacity;
+	
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdOn;
+
+	@UpdateTimestamp
+	private LocalDateTime lastUpdatedOn;
+	
+	@Column(nullable = false)
+	private Boolean isDeleted;
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "activity", cascade = CascadeType.ALL)
+	private List<Ticket> tickets = new ArrayList<>();
+	
+}
---
+++ WonderWorld_Park_BK/src/main/java/com/masai/model/Role.java
@@ -0,0 +1,16 @@
+package com.masai.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public enum Role {
+	
+	ADMIN("ROLE_ADMIN"),
+	USER("ROLE_USER");
+	
+	private final String roleName;
+}
