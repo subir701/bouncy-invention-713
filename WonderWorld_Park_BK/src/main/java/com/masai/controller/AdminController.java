d2bb6cc5475c49f0cce9fa89ee4c07997c6642f8
subir701 <subiradhikari181@gmail.com>
2023-08-24 17:43:43 +0530

    customer

--- WonderWorld_Park_BK/src/main/java/com/masai/controller/AdminController.java
+++ WonderWorld_Park_BK/src/main/java/com/masai/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.masai.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.Exception.AdminException;
+import com.masai.Exception.CustomerException;
 import com.masai.model.Admin;
+import com.masai.model.Customer;
 import com.masai.service.AdminService;
+import com.masai.service.CustomerService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
@@ -23,6 +29,9 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 	
+	@Autowired
+	private CustomerService customerService;
+	
 	@PostMapping("/register")
 	public ResponseEntity<Admin> registerAdmin(@Valid @RequestBody Admin admin)throws AdminException{
 		return new ResponseEntity<Admin>(adminService.insertAdmin(admin),HttpStatus.CREATED);
@@ -33,5 +42,13 @@ public class AdminController {
 		return new ResponseEntity<Admin>(adminService.deleteAdmin(adminId),HttpStatus.OK);
 	}
 	
+	@GetMapping("/customers")
+	public ResponseEntity<List<Customer>> getAllCustomer()throws CustomerException{
+		return new ResponseEntity<List<Customer>>(customerService.viewAllCustomer(),HttpStatus.FOUND);
+	}
 	
+	@GetMapping("/customers/{customerId}")
+	public ResponseEntity<Customer> getCustomerById(@PathVariable Integer customerId)throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.viewCustomerById(customerId),HttpStatus.FOUND);
+	}
 }
--- WonderWorld_Park_BK/src/main/java/com/masai/controller/CustomerController.java
+++ WonderWorld_Park_BK/src/main/java/com/masai/controller/CustomerController.java
@@ -1,10 +1,45 @@
 package com.masai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.Exception.CustomerException;
+import com.masai.model.Customer;
+import com.masai.service.CustomerService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/wonderWorld/customers")
 public class CustomerController {
 
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer)throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.registerCustomer(customer),HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update/{customerId}")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer,@PathVariable Integer customerId)throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.updateCustomer(customer, customerId),HttpStatus.ACCEPTED);
+	}
+	
+	@PutMapping("/delete/{customerId}")
+	public ResponseEntity<Customer> deleteCustomer(@PathVariable Integer customerId)throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.deleteCustomer(customerId),HttpStatus.ACCEPTED);
+	}
+	
+	@GetMapping("/{customerId}")
+	public ResponseEntity<Customer> getCustomerById(@PathVariable Integer customerId)throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.viewCustomerById(customerId),HttpStatus.FOUND);
+	}
 }
