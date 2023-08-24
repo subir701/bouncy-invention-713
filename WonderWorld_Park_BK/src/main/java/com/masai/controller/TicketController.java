91827c174bae5c6706b11008b0ab1b961f0fb82e
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-08-24 18:25:58 +0530

    Ticket

--- WonderWorld_Park_BK/src/main/java/com/masai/controller/TicketController.java
+++ WonderWorld_Park_BK/src/main/java/com/masai/controller/TicketController.java
@@ -8,15 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.DTO.TicketDTO;
 import com.masai.model.Customer;
-import com.masai.model.Ticket;
 import com.masai.repository.CustomerRepository;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/ticket")
